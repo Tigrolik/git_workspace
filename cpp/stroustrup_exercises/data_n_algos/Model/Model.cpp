0cde4521fe37c5eb98fefde1ba70202b97a78666
Tigrolik <ivan.martynov@lut.fi>
2016-05-04 18:00:30 +0300

    Modified Model

--- cpp/stroustrup_exercises/data_n_algos/Model/Model.cpp
+++ cpp/stroustrup_exercises/data_n_algos/Model/Model.cpp
@@ -1,23 +1,20 @@
+#include <algorithm>
 #include "Model.h"
 
 /*
  * ------------------ Vec_base implementation ------------------
  */
 template <class T, size_t N>
+Vec_base<T, N>::Vec_base(): a_() {
+}
+
+template <class T, size_t N>
 Vec_base<T, N>::Vec_base(const std::array<T,N> &a): a_(a) {
-/* note: init with the regular brackets (not curved) */
+    /* note: init with the regular brackets (not curved) */
 }
 
 template <class T, size_t N>
 Vec_base<T, N>::Vec_base(const Vec_base &v): a_(v.a_) {
-}
-
-template <class T, size_t N>
-Vec_base<T, N> Vec_base<T, N>::operator+(const Vec_base<T, N> &v) {
-    std::array<T, N> a;
-    for (size_t i {0}; i < N; ++i)
-        a[i] = a_[i] + v[i];
-    return a;
 }
 
 template <class T, size_t N>
@@ -31,17 +28,108 @@ std::ostream& operator<<(std::ostream &os, const Vec_base<T, N> &v) {
 /*
  * ------------------ Vertex implementation ------------------
  */
+Vertex::Vertex(): Vec_base {} { }
+
 Vertex::Vertex(const double a, const double b, const double c):
     Vec_base {std::array<double, 3> {a, b, c}} {
     }
 
 Vertex::Vertex(const Vec_base<double, 3> &v): Vec_base {v} { }
 
-Vertex  Vertex::operator+(const Vertex &v) {
-    return Vertex {Vec_base<double, 3>::operator+(v)};
+std::istream& operator>>(std::istream &is, Vertex &v) {
+    double a, b, c;
+    is >> a >> b >> c;
+    if (is) v = Vertex {a, b, c};
+    return is;
 }
 
 std::ostream& operator<<(std::ostream &os, const Vertex &v) {
     return os << (Vec_base<double, 3>&)v;
+}
+
+/*
+ * ------------------ Point3 implementation ------------------
+ */
+Point3::Point3(): Vec_base {} { }
+
+Point3::Point3(const int a, const int b, const int c):
+    Vec_base {std::array<int, 3> {a, b, c}} {
+    }
+
+Point3::Point3(const Vec_base<int, 3> &v): Vec_base {v} { }
+
+Point3::Point3(const Vec_base<double, 3> &v): Vec_base {} {
+    for (int i {0}; i < 3; ++i)
+        a_[i] = round(v[i]);
+}
+
+std::istream& operator>>(std::istream &is, Point3 &p) {
+    int a, b, c;
+    char ch;
+    is >> a >> ch >> b >> ch >> c;
+    if (is) p = Point3 {--a, --b, --c};
+    return is;
+}
+
+
+std::ostream& operator<<(std::ostream &os, const Point3 &p) {
+    return os << (Vec_base<int, 3>&)p;
+}
+
+/*
+ * ------------------ Point3 implementation ------------------
+ */
+Facet::Facet(): f_() { }
+
+Facet::Facet(const Point3 &p1, const Point3 &p2, const Point3 &p3):
+    f_(std::array<Point3, 3> {p1, p2, p3}) {
+}
+
+std::istream& operator>>(std::istream &is, Facet &f) {
+    Point3 p1, p2, p3;
+    is >> p1 >> p2 >> p3;
+    if (is) f = Facet {p1, p2, p3};
+    return is;
+}
+
+/*
+ * ------------------ Model implementation ------------------
+ */
+Model::Model(const std::string &fn): verts_{}, faces_{}, norms_{}, texverts_{} {
+    std::ifstream ifs {fn};
+    if (!ifs)
+        throw std::runtime_error("cannot open file " + fn);
+    ifs.exceptions(ifs.exceptions() | std::ios_base::badbit);
+
+    for (std::string s; ifs >> s;) {
+        if (s == "v") {
+            Vertex v; ifs >> v;
+            verts_.push_back(v);
+        }
+        if (s == "f") {
+            // reading the triples: (vertex / texvertex / normal)
+            Facet f; ifs >> f;
+            faces_.push_back(f);
+        }
+        if (s == "vn") {
+            // reading vertex normals
+            Normal nv; ifs >> nv;
+            norms_.push_back(nv);
+        }
+        if (s == "vt") {
+            // reading texture vertices
+            Vertex texv; ifs >> texv;
+            texverts_.push_back(texv);
+        }
+        if (ifs.fail()) {
+            ifs.unget();
+            ifs.clear(std::ios_base::failbit);
+        }
+    }
+}
+
+Vertex Model::texvertex(const int iface, const int ivert) const {
+    const int idx {faces_[iface][ivert][1]};
+    return Vertex {texverts_[idx][0], texverts_[idx][1], texverts_[idx][2]};
 }
 
--- cpp/stroustrup_exercises/data_n_algos/Model/Model.h
+++ cpp/stroustrup_exercises/data_n_algos/Model/Model.h
@@ -2,8 +2,8 @@
 #define _MODEL_H_
 
 #include <fstream>
-#include <vector>
 #include <array>
+#include <vector>
 
 /*
  * Base class for arrays of fixed size.
@@ -18,31 +18,105 @@ std::ostream& operator<<(std::ostream&, const Vec_base<T, N>&);
 
 template <class T, size_t N>
 class Vec_base {
+//protected:
 public:
+    Vec_base();
     Vec_base(const std::array<T,N>&);
     Vec_base(const Vec_base&);
 
     virtual ~Vec_base() { }
-
-    Vec_base operator+(const Vec_base&);
 
     friend std::ostream& operator<< <> (std::ostream&, const Vec_base&);
 
     T operator[](const int i) { return a_[i]; }
     const T& operator[](const int i) const { return a_[i]; }
 
-private:
+    size_t size() const { return N; }
+
+protected:
     std::array<T, N> a_;
 };
 
+template <class T, class U, size_t N>
+Vec_base<typename std::common_type<T, U>::type, N> operator+(const
+        Vec_base<T, N> &lhs, const Vec_base<U, N> &rhs) {
+    std::array<typename std::common_type<T, U>::type, N> a;
+    for (size_t i {0}; i < N; ++i)
+        a[i] = lhs[i] + rhs[i];
+    return Vec_base<typename std::common_type<T, U>::type, N> {a};
+}
+
 class Vertex: public Vec_base<double, 3> {
 public:
+    Vertex();
     Vertex(const double, const double, const double);
     Vertex(const Vec_base<double, 3>&);
 
-    Vertex operator+(const Vertex&);
+    friend std::istream& operator>>(std::istream&, Vertex&);
 
-    friend std::ostream& operator<<(std::ostream&, const Vertex&);
+    double x() const { return a_[0]; }
+    double y() const { return a_[1]; }
+    double z() const { return a_[2]; }
+};
+
+/*
+ * class Normal
+ * Basically this class copies Vertex, the reason to have it is merely for a
+ * name and with possible addition of specific methods
+ */
+class Normal: public Vertex { };
+
+class Point3: public Vec_base<int, 3> {
+public:
+    Point3();
+    Point3(const int, const int, const int);
+    Point3(const Vec_base<int, 3>&);
+    Point3(const Vec_base<double, 3>&);
+
+    friend std::istream& operator>>(std::istream&, Point3&);
+
+    int x() const { return a_[0]; }
+    int y() const { return a_[1]; }
+    int z() const { return a_[2]; }
+};
+
+class Facet {
+public:
+    Facet();
+    Facet(const Point3&, const Point3&, const Point3&);
+
+    Point3 operator[](const int i) { return f_[i]; }
+    const Point3& operator[](const int i) const { return f_[i]; }
+
+    friend std::istream& operator>>(std::istream&, Facet&);
+
+private:
+    std::array<Point3, 3> f_;
+};
+
+class Model {
+public:
+    Model(const std::string &);
+
+    ~Model() = default;
+
+    const Vertex vertex(const int i) const { return verts_[i]; }
+    Point3 face(const int idx) const {
+        return {faces_[idx][0][0], faces_[idx][1][0], faces_[idx][2][0]};
+    }
+    Vertex texvertex(const int, const int) const;
+    const Normal normal(const int i) const { return norms_[i]; }
+
+    size_t num_vertices() const { return verts_.size(); }
+    size_t num_faces() const { return faces_.size(); }
+    size_t num_normals() const { return norms_.size(); }
+    size_t num_texvertices() const { return texverts_.size(); }
+
+private:
+    std::vector<Vertex> verts_;
+    std::vector<Facet> faces_;
+    std::vector<Normal> norms_;
+    std::vector<Vertex> texverts_; // texture vertices
 };
 
 #endif
